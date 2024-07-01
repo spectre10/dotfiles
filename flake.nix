a160de026542c71a411b436832ddb48221dc426d
Ghanshyam Thakkar <shyamthakkar001@gmail.com>
2024-07-01 23:35:33 +0530

    initial configuration
    Signed-off-by: Ghanshyam Thakkar <shyamthakkar001@gmail.com>

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Nix Config";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-24.05";
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    # use the following for unstable:
+    # nixpkgs.url = "nixpkgs/nixos-unstable";
+
+    # or any branch you want:
+    # nixpkgs.url = "nixpkgs/{BRANCH-NAME}";
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }:
+  let
+    lib = nixpkgs.lib;
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    nixosConfigurations = {
+      nixos = lib.nixosSystem {
+        modules = [
+          ./configuration.nix
+          home-manager.nixosModules.home-manager {
+            imports = [ ./home.nix ];
+          }
+        ];
+      };
+    };
+  };
+}
---
+++ home.nix
@@ -0,0 +1,28 @@
+{config, pkgs, ...}:
+
+{
+  home-manager.useGlobalPkgs = true;
+  home-manager.useUserPackages = true;
+  home-manager.users.sp = { pkgs, ... }: {
+    programs.zsh.enable = true;
+    programs.starship.enable = true;
+    programs.git = {
+      enable = true;
+      userName = "Ghanshyam Thakkar";
+      userEmail = "shyamthakkar001@gmail.com";
+      extraConfig = {
+        init.defaultBranch = "main";
+      };
+    };
+    home.packages = [
+      pkgs.hello
+    ];
+    home.username = "sp";
+    home.homeDirectory = "/home/sp";
+  # The state version is required and should stay at the version you
+  # originally installed.
+    home.stateVersion = "24.05";
+  };
+}
+
+
